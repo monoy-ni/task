29f55ecf2fb8346e221a24943fa22145b29f6a55
monoy-ni <18267532095@163.com>
2026-04-17 14:14:24 +0800

    chore: reorganize project structure

--- frontend/src/components/mono/index.ts
+++
@@ -1,5 +0,0 @@
-export { MonoAvatar } from './MonoAvatar';
-export { MonoMessage } from './MonoMessage';
-export { MonoFloatingAssistant } from './MonoFloatingAssistant';
-export { MonoGreeting } from './MonoGreeting';
-export { monoQuotes, getMonoQuote, getGreeting } from './monoQuotes';
